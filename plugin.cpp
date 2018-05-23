9b365b5f021c2a9005ce4838862ebec5dcf65a50
Federico Ferri <federico.ferri.it@gmail.com>
2018-05-23 22:58:46 +0200

    parser output in XML

--- plugin.cpp
+++ plugin.cpp
@@ -9,6 +9,7 @@
 #include "stack/stackString.h"
 #include "stack/stackArray.h"
 #include "stack/stackMap.h"
+#include "tinyxml2.h"
 #include <ostream>
 #include <sstream>
 #include <stdexcept>
@@ -16,84 +17,78 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/optional.hpp>
 
-struct ASTNodeLocation
-{
-    int line;
-    int column;
-    int offset;
-};
+using namespace tinyxml2;
 
-struct ASTNode
+void extractNode(XMLDocument *doc, XMLElement *parent, CStackObject *obj)
 {
-    std::string tag;
-    boost::optional<ASTNodeLocation> location;
-    std::vector<ASTNode> children;
-};
-
-ASTNodeLocation extractLocation(CStackMap *map)
-{
-    ASTNodeLocation ret;
-    ret.line = map->getInt("line");
-    ret.column = map->getInt("column");
-    ret.offset = map->getInt("offset");
-    return ret;
-}
-
-ASTNode extractNode(CStackObject *obj)
-{
-    ASTNode ret;
     if(obj->getObjectType() == STACK_ARRAY)
     {
         // sequence of nodes
-        ret.tag = "sequence";
         CStackArray *arr = static_cast<CStackArray*>(obj);
         const std::vector<CStackObject*> *objs = arr->getObjects();
+        XMLElement *e = doc->NewElement("group");
         for(CStackObject *obj : *objs)
         {
-            ret.children.push_back(extractNode(obj));
+            extractNode(doc, e, obj);
         }
+        parent->InsertEndChild(e);
     }
     else if(obj->getObjectType() == STACK_MAP)
     {
         // single node
-        CStackMap *map = static_cast<CStackMap*>(obj);
-        ret.tag = map->contains("tag", STACK_STRING) ? map->getString("tag") : "sequence";
-        ret.location = extractLocation(map->getMap("location"));
-        for(const auto &item : *map->getKeyValuePairsKInt())
+        CStackMap *oMap = static_cast<CStackMap*>(obj);
+        XMLElement *e = doc->NewElement(oMap->getString("tag").c_str());
+
+        CStackMap *oLoc = oMap->getMap("location");
+        XMLElement *loc = doc->NewElement("location");
+        loc->SetAttribute("line", oLoc->getInt("line"));
+        loc->SetAttribute("column", oLoc->getInt("column"));
+        loc->SetAttribute("offset", oLoc->getInt("offset"));
+        e->InsertEndChild(loc);
+
+        CStackMap *oELoc = oMap->getMap("end_location");
+        if(oELoc)
         {
-            ret.children.push_back(extractNode(item.second));
+            XMLElement *eloc = doc->NewElement("end-location");
+            eloc->SetAttribute("line", oELoc->getInt("line"));
+            eloc->SetAttribute("column", oELoc->getInt("column"));
+            eloc->SetAttribute("offset", oELoc->getInt("offset"));
+            e->InsertEndChild(eloc);
         }
+
+        for(const auto &item : *oMap->getKeyValuePairsKInt())
+        {
+            extractNode(doc, e, item.second);
+        }
+        parent->InsertEndChild(e);
     }
     else if(obj->getObjectType() == STACK_STRING)
     {
         // literal
         CStackString *str = static_cast<CStackString*>(obj);
-        ret.tag = str->getValue();
+        parent->SetAttribute("value", str->getValue().c_str());
     }
     else
     {
         throw std::runtime_error((boost::format("bad object type: %d (%s)") % obj->getObjectType() % static_cast<CStackString*>(obj)->getValue()).str());
     }
-    return ret;
 }
 
-ASTNode parseLuaCode(std::string code)
+void parse(SScriptCallBack *p, const char *cmd, parse_in *in, parse_out *out)
 {
-    int scriptHandleOrType = sim_scripttype_sandboxscript;
-
     simInt stackHandle = simCreateStack();
     if(stackHandle == -1)
         throw std::runtime_error("failed to create a stack");
 
     std::string req = "require 'luacheck.parser'@";
-    simInt ret0 = simExecuteScriptString(scriptHandleOrType, req.c_str(), stackHandle);
+    simInt ret0 = simExecuteScriptString(sim_scripttype_sandboxscript, req.c_str(), stackHandle);
     if(ret0 == -1)
         throw std::runtime_error("failed to load luacheck.parser");
 
     std::string delim = "========================================================";
-    code = (boost::format("package.loaded['luacheck.parser'].parse[%s[%s]%s]@") % delim % code % delim).str();
+    std::string code = (boost::format("package.loaded['luacheck.parser'].parse[%s[%s]%s]@") % delim % in->code % delim).str();
 
-    simInt ret = simExecuteScriptString(scriptHandleOrType, code.c_str(), stackHandle);
+    simInt ret = simExecuteScriptString(sim_scripttype_sandboxscript, code.c_str(), stackHandle);
     if(ret == -1)
     {
         CStackObject *obj = CStackObject::buildItemFromTopStackPosition(stackHandle);
@@ -104,42 +99,17 @@ ASTNode parseLuaCode(std::string code)
         throw std::runtime_error("empty result in stack");
 
     CStackObject *obj = CStackObject::buildItemFromTopStackPosition(stackHandle);
-    ASTNode node = extractNode(obj);
+    XMLDocument *doc = new XMLDocument;
+    XMLElement *root = doc->NewElement("ast");
+    extractNode(doc, root, obj);
+    doc->InsertFirstChild(root);
+
+    XMLPrinter printer;
+    doc->Print(&printer);
+    out->result = printer.CStr();
+    delete doc;
 
     simReleaseStack(stackHandle);
-
-    return node;
-}
-
-void dumpSyntaxTree(std::ostream &os, ASTNode &n, bool showLocation = false, std::string indent = "", std::string indent_elem = "    ")
-{
-    std::string indent0 = indent;
-    std::string indent1 = indent + indent_elem;
-    os <<            n.tag;
-    if((showLocation && n.location) || n.children.size()>0)
-        os << " {" << std::endl;
-    if(showLocation && n.location)
-    {
-        os << indent1 << "location.line="   << n.location->line << std::endl;
-        os << indent1 << "location.column=" << n.location->column << std::endl;
-        os << indent1 << "location.offset=" << n.location->offset << std::endl;
-    }
-    for(int i = 0; i < n.children.size(); i++)
-    {
-        os << indent1;
-        dumpSyntaxTree(os, n.children[i], showLocation, indent1);
-    }
-    if((showLocation && n.location) || n.children.size()>0)
-        os << indent0 << "}";
-    os << std::endl;
-}
-
-void parse(SScriptCallBack *p, const char *cmd, parse_in *in, parse_out *out)
-{
-    ASTNode ast = parseLuaCode(in->code);
-    std::stringstream ss;
-    dumpSyntaxTree(ss, ast);
-    out->result = ss.str();
 }
 
 class Plugin : public vrep::Plugin
