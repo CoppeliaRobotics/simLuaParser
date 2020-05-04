b20e12a11e7891597834f41ca8f2e26330cf338e
Federico Ferri <federico.ferri.it@gmail.com>
2020-05-04 15:46:02 +0200

    use module info api

--- plugin.cpp
+++ plugin.cpp
@@ -186,8 +186,8 @@ public:
         if(!registerScriptStuff())
             throw std::runtime_error("failed to register script stuff");
 
-        simSetModuleInfo(PLUGIN_NAME, 0, "Lua Parser Plugin", 0);
-        simSetModuleInfo(PLUGIN_NAME, 1, BUILD_DATE, 0);
+        setExtVersion("Lua Parser Plugin");
+        setBuildDate(BUILD_DATE);
     }
 };
 
